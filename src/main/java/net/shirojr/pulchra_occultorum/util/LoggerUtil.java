25d3fd1b05d0488ee057d96e6afad5b85b127a2a
JR1811 <janreichelt77@gmail.com>
2024-06-07 16:39:13 +0200

    small LoggerUtil clean-up

--- src/main/java/net/shirojr/pulchra_occultorum/util/LoggerUtil.java
+++ src/main/java/net/shirojr/pulchra_occultorum/util/LoggerUtil.java
@@ -2,6 +2,7 @@ package net.shirojr.pulchra_occultorum.util;
 
 import net.fabricmc.loader.api.FabricLoader;
 import net.shirojr.pulchra_occultorum.PulchraOccultorum;
+import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,12 +15,12 @@ public class LoggerUtil {
     /**
      * Method to display useful information in the console at runtime.<br>
      * The information will only be printed, if the instance is running in a developer environment.<br><br>
-     * <i>Override of the {@link #devLogger(String, boolean, Exception) devLogger} method.</i>
+     * <i>Override of the {@link #devLogger(String, Type, Exception) devLogger} method.</i>
      *
      * @param input Informative text of the current state of the mod, to display in the game console
      */
     public static void devLogger(String input) {
-        devLogger(input, false, null);
+        devLogger(input, Type.INFO, null);
     }
 
     /**
@@ -30,17 +31,20 @@ public class LoggerUtil {
      * @param isError   Will display texts differently in the instance's console
      * @param exception If not available, pass over a <b><i>null</i></b> value
      */
-    public static void devLogger(String input, boolean isError, Exception exception) {
+    public static void devLogger(String input, Type type, @Nullable Exception exception) {
         if (!FabricLoader.getInstance().isDevelopmentEnvironment()) return;
         String printText = "DEV - [ " + input + " ]";
-        if (!isError) {
-            LOGGER.info(printText);
-            return;
+        switch (type) {
+            case INFO -> LOGGER.info(printText);
+            case WARNING -> LOGGER.warn(printText);
+            case ERROR -> {
+                if (exception == null) LOGGER.error(printText);
+                else LOGGER.error(printText, exception);
+            }
         }
-        if (exception == null) {
-            LOGGER.error(printText);
-            return;
-        }
-        LOGGER.error(printText, exception);
+    }
+
+    public enum Type {
+        INFO, WARNING, ERROR;
     }
 }
