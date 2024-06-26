62b0643f58ea7030aa85b4c657f142d89c16219e
JR1811 <janreichelt77@gmail.com>
2024-06-26 19:30:57 +0200

    fixed lazy item group registration

--- src/main/java/net/shirojr/pulchra_occultorum/init/ItemGroups.java
+++ src/main/java/net/shirojr/pulchra_occultorum/init/ItemGroups.java
@@ -1,10 +1,10 @@
 package net.shirojr.pulchra_occultorum.init;
 
+import com.google.common.base.Supplier;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
@@ -13,15 +13,15 @@ import net.minecraft.text.Text;
 import net.shirojr.pulchra_occultorum.PulchraOccultorum;
 import net.shirojr.pulchra_occultorum.util.LoggerUtil;
 
-@SuppressWarnings("unused")
+@SuppressWarnings("Convert2MethodRef")
 public class ItemGroups {
-    public static final RegistryKey<ItemGroup> ITEMS = registerItemGroup("items", Items.STICK);
-    public static final RegistryKey<ItemGroup> BLOCKS = registerItemGroup("blocks", Items.STICK);
+    public static final RegistryKey<ItemGroup> ITEMS = registerItemGroup("items", () -> Items.TAROT_MONOLITH);
+    public static final RegistryKey<ItemGroup> BLOCKS = registerItemGroup("blocks", () -> Blocks.SPOTLIGHT_LAMP.asItem());
 
 
-    public static RegistryKey<ItemGroup> registerItemGroup(String name, Item icon) {
+    public static RegistryKey<ItemGroup> registerItemGroup(String name, Supplier<Item> icon) {
         Text displayName = Text.translatable("itemgroup.%s.%s".formatted(PulchraOccultorum.MOD_ID, name));
-        ItemGroup itemGroup = FabricItemGroup.builder().icon(() -> new ItemStack(icon)).displayName(displayName).build();
+        ItemGroup itemGroup = FabricItemGroup.builder().icon(() -> new ItemStack(icon.get())).displayName(displayName).build();
         Registry.register(Registries.ITEM_GROUP, getRegistryKey(name), itemGroup);
         return getRegistryKey(name);
     }
